66066f9622d48ce33f50235820f68297f2cf84e8
Priyanshu Bharti <46714242+priyanshu-bharti@users.noreply.github.com>
2025-03-18 23:20:53 +0530

    Added Basics

---
+++ Basics.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+
+class Basics {
+  public static void main(String[] args) {
+
+    /* ----- Primitives ----- */
+
+    // Bytes
+    byte b = 100;
+    System.out.println(b);
+
+    // Short
+    short s = 10000;
+    System.out.println(s);
+
+    // Int
+    int i = 100000000;
+    System.out.println(i);
+
+    // Long
+    long l = 100000000000L;
+    System.out.println(l);
+
+    // Float
+    float f = 1.42f;
+    System.out.println(f);
+
+    // Double
+    double d = 123.12345345233;
+    System.out.println(d);
+
+    // Char
+    char c = 'a';
+    System.out.println(c);
+
+    // Boolean
+    boolean t = true;
+    System.out.println(t);
+
+    /* ----- Non-Primitives ----- */
+
+    // Strings
+    String str = "Hello, World!";
+    System.out.println(str);
+
+    // Arrays
+    int[] array = new int[5];
+    System.out.println(Arrays.toString(array));
+
+    // Enums
+    enum Day {
+      SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+    }
+    Day day = Day.MONDAY;
+    System.out.println(day);
+  }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This is a repo containing my notes for learning java. Once I am done writing these notes, I'll update the Readme to something meaninful.
